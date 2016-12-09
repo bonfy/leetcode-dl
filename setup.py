278ebbc2e174b31cf31ed2f8a28295accc0b7737
bonfy <foreverbonfy@163.com>
2016-12-09 22:30:48 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='leetcode-dl',
+    version='0.0.1',
+    keywords=('leetcode', 'downloader'),
+    description='Command-line program to download solutions from leetcode.com',
+    license='MIT License',
+    install_requires=None,
+
+    author='bonfy',
+    author_email='foreverbonfy@163.com',
+
+    packages=find_packages(),
+    platforms='any',
+)
