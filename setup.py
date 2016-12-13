0385109864776e5ada6cda1d4e33ea561a6806da
bonfy <foreverbonfy@163.com>
2016-12-13 21:51:17 +0800

    add readme to pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from codecs import open
 
 name = "leetcode-dl"
 packages = [
@@ -6,15 +7,18 @@ packages = [
     "leetcode_dl_cli"
 ]
 
+with open('README.rst', 'r', 'utf-8') as f:
+    readme = f.read()
 
 setup(
     name=name,
-    version="0.1.2",
+    version="0.1.3",
     keywords="leetcode download",
     description="Command-line program to download solutions from leetcode.com",
     download_url="https://pypi.python.org/pypi/{}".format(name),
-    long_description="https://github.com/bonfy/{}".format(name),
+    long_description=readme,
     license="MIT License",
+    url='http://github.com/bonfy/{}'.format(name),
     author="bonfy",
     author_email="foreverbonfy@163.com",
     install_requires=[
@@ -25,14 +29,14 @@ setup(
     packages=packages,
     entry_points={"console_scripts": "leetcode-dl=leetcode_dl_cli.main:main"},
     platforms="any",
-    classifiers=(
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5'
-    ),
+    # classifiers=(
+    #     'Development Status :: 4 - Beta',
+    #     'Intended Audience :: Developers',
+    #     'License :: OSI Approved :: MIT License',
+    #     'Programming Language :: Python',
+    #     'Programming Language :: Python :: 3',
+    #     'Programming Language :: Python :: 3.3',
+    #     'Programming Language :: Python :: 3.4',
+    #     'Programming Language :: Python :: 3.5'
+    # ),
 )
