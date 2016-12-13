29352d7a98019a45ba5c594d0c96dcc40d9cd5f7
bonfy <foreverbonfy@163.com>
2016-12-13 17:35:29 +0800

    add version 0.1.0

--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+name = 'leetcode-dl'
+packages = [
+    "leetcode_dl",
+    "leetcode_dl_cli"
+]
 
 
 setup(
-    name='leetcode-dl',
-    version='0.0.1',
-    keywords=('leetcode', 'downloader'),
+    name=name,
+    version='0.1.0',
+    keywords=('leetcode', 'download'),
     description='Command-line program to download solutions from leetcode.com',
+    download_url='https://pypi.python.org/pypi/{}'.format(name),
+    long_description='https://github.com/bonfy/{}'.format(name),
     license='MIT License',
-    install_requires=None,
-
     author='bonfy',
     author_email='foreverbonfy@163.com',
-
-    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'clint',
+        'pyquery'
+    ],
+    packages=packages,
     platforms='any',
+    entry_points={'console_scripts': 'leetcode-dl=leetcode_dl_cli.main:main'},
 )
